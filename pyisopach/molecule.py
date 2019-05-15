4fd5eb44a6f68ee7384eec51ce64b53d265c6b40
KeironO <keiron.oshea@gmail.com>
2019-05-15 19:59:34 +0100

    Stripped

--- pyisopach/molecule.py
+++ pyisopach/molecule.py
@@ -38,7 +38,7 @@ class Molecule:
          return structure_dict
 
 
-    def _rule_dict_elements_list(self, rule_dict):
+    def _apply_rule_dict(self, rule_dict):
         _structure_dict = self.structure_dict
 
         if "multiply" in rule_dict.keys():
@@ -64,72 +64,3 @@ class Molecule:
                 continue
 
         return self._element_list
-
-    def isotopic_distribution(self, rule_dict=None, electrons=1, charge=0):
-        if charge == 0:
-            charge = 1
-
-        if rule_dict == None:
-            elements_list = self._as_elements
-        else:
-            elements_list = self._rule_dict_elements_list(rule_dict)
-
-        def cartesian(ratios,
-                      weights,
-                      f_ratios,
-                      f_weights,
-                      count=1,
-                      threshold=0.05):
-            n_ratios = []
-            n_weights = []
-            normalised_ratio = [n / max(f_ratios) for n in f_ratios]
-
-            for i in enumerate(ratios[count]):
-                r = ratios[count][i[0]]
-                w = weights[count][i[0]]
-                for j in enumerate(normalised_ratio):
-                    current_n_ratio = normalised_ratio[j[0]] * 100
-                    current_s_weight = f_weights[j[0]]
-                    t_w = current_n_ratio * r
-                    if t_w > threshold:
-                        n_ratios += [current_n_ratio * r]
-                        n_weights += [current_s_weight + w]
-            count = count + 1
-            if count < len(ratios) and len(n_ratios) < 1000:
-                n_ratios, n_weights = cartesian(ratios, weights, n_ratios,
-                                                n_weights, count)
-            return n_ratios, n_weights
-
-        def isotopes():
-            weights = []
-            ratios = []
-            for element in elements_list:
-                for i in range(element.count):
-                    weights.append(element.isotopic_weight)
-                    ratios.append(element.isotopic_ratios)
-            return cartesian(ratios, weights, ratios[0], weights[0])
-
-        def calculate_distributions(gen_iso):
-            signals = dict((key, tuple(v for (k, v) in pairs))
-                           for (key, pairs) in itertools.groupby(
-                               sorted(gen_iso), operator.itemgetter(0)))
-
-            distributions = {}
-            peak_intensity = max(signals.values())[0]
-
-            for mass, intensity in signals.items():
-                mass = round(
-                    (mass + (ELECTRON_WEIGHT * electrons)) / abs(charge), 5)
-                relative_intensity = round(
-                    float(sum(intensity)) * 100 / peak_intensity, 5)
-                if mass not in distributions.keys():
-                    distributions[mass] = relative_intensity
-                else:
-                    distributions[mass] += relative_intensity
-            return sorted(distributions.items(), key=lambda x: x[0])
-
-        ratios, weights = isotopes()
-        distributions = calculate_distributions(
-            [(weights[index], ratio) for index, ratio in enumerate(ratios)
-             if ratio > 1e-6])
-        return distributions
