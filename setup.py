e08b896bb70f2139404ad70487f82b6f53c914ee
KeironO <keiron.oshea@gmail.com>
2019-08-10 14:40:35 +0100

    Refactoring the setup file, and move vn up ready for release

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-from setuptools import setup; setup()
+from setuptools import setup;
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    install_requires=required
+)
